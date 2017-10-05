8e2ee35eb8cc5f8d927008afc16654708c5c9004
dtekluva <31896598+dtekluva@users.noreply.github.com>
2017-10-05 14:33:27 +0100

    added new line to print

--- area of square code.py
+++ area of square code.py
@@ -2,5 +2,5 @@ length=int(input("please enter length: "))
 breadth=int(input("please enter breadth: "))
 area=length*breadth
 
-print("The Area of the square with sides ",length,"cm and", breadth, "is : ", area)
+print("\n The Area of the square with sides ",length,"cm and", breadth, "is : ", area)
 input()
